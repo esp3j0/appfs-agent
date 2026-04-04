esp3j0/appfs-platform

Add explicit provider configuration support

rust/crates/api/src/client.rs
@@ -25,9 +25,17 @@ pub enum ProviderClient {
     OpenAi(OpenAiCompatClient),
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ProviderOverride {
+    pub provider: ProviderKind,
+    pub base_url: Option<String>,
+    pub api_key_env: Option<String>,
+    pub auth_token_env: Option<String>,
+}
+
 impl ProviderClient {
     pub fn from_model(model: &str) -> Result<Self, ApiError> {
-        Self::from_model_with_default_auth(model, None)
+        Self::from_model_with_provider_override(model, None)
     }
 
     pub fn from_model_with_default_auth(
@@ -37,13 +45,67 @@ impl ProviderClient {
         let resolved_model = providers::resolve_model_alias(model);
         match providers::detect_provider_kind(&resolved_model) {
             ProviderKind::ClawApi => Ok(Self::ClawApi(match default_auth {
-                Some(auth) => ClawApiClient::from_auth(auth),
+                Some(auth) => ClawApiClient::from_auth(auth).with_base_url(read_base_url()),
                 None => ClawApiClient::from_env()?,
             })),
             ProviderKind::Xai => Ok(Self::Xai(OpenAiCompatClient::from_env(
                 OpenAiCompatConfig::xai(),
             )?)),
             ProviderKind::OpenAi => Ok(Self::OpenAi(OpenAiCompatClient::from_env(
+                OpenAiCompatConfig::openai(),
+            )?)),
+        }
+    }
+
+    pub fn from_model_with_provider_override(
+        model: &str,
+        provider_override: Option<&ProviderOverride>,
+    ) -> Result<Self, ApiError> {
+        Self::from_model_with_provider_override_and_claw_auth_resolver(
+            model,
+            provider_override,
+            claw_provider::AuthSource::from_env_or_saved,
+        )
+    }
+
+    pub fn from_model_with_claw_auth_resolver<F>(
+        model: &str,
+        provider_override: Option<&ProviderOverride>,
+        resolve_claw_auth: F,
+    ) -> Result<Self, ApiError>
+    where
+        F: FnOnce() -> Result<AuthSource, ApiError>,
+    {
+        Self::from_model_with_provider_override_and_claw_auth_resolver(
+            model,
+            provider_override,
+            resolve_claw_auth,
+        )
+    }
+
+    pub fn from_model_with_provider_override_and_claw_auth_resolver<F>(
+        model: &str,
+        provider_override: Option<&ProviderOverride>,
+        resolve_claw_auth: F,
+    ) -> Result<Self, ApiError>
+    where
+        F: FnOnce() -> Result<AuthSource, ApiError>,
+    {
+        let resolved_model = providers::resolve_model_alias(model);
+        let provider_kind = provider_override
+            .map(|config| config.provider)
+            .unwrap_or_else(|| providers::detect_provider_kind(&resolved_model));
+        match provider_kind {
+            ProviderKind::ClawApi => Ok(Self::ClawApi(build_claw_client(
+                provider_override,
+                resolve_claw_auth,
+            )?)),
+            ProviderKind::Xai => Ok(Self::Xai(build_openai_compat_client(
+                provider_override,
+                OpenAiCompatConfig::xai(),
+            )?)),
+            ProviderKind::OpenAi => Ok(Self::OpenAi(build_openai_compat_client(
+                provider_override,
                 OpenAiCompatConfig::openai(),
             )?)),
         }
@@ -80,6 +142,92 @@ impl ProviderClient {
                 .await
                 .map(MessageStream::OpenAiCompat),
         }
+    }
+}
+
+fn build_claw_client<F>(
+    provider_override: Option<&ProviderOverride>,
+    resolve_default_auth: F,
+) -> Result<ClawApiClient, ApiError>
+where
+    F: FnOnce() -> Result<AuthSource, ApiError>,
+{
+    let Some(provider_override) = provider_override else {
+        return Ok(ClawApiClient::from_auth(resolve_default_auth()?).with_base_url(read_base_url()));
+    };
+
+    let auth = if provider_override.api_key_env.is_none() && provider_override.auth_token_env.is_none()
+    {
+        resolve_default_auth()?
+    } else {
+        resolve_claw_auth_from_override(provider_override)?
+    };
+    let base_url = provider_override
+        .base_url
+        .clone()
+        .unwrap_or_else(read_base_url);
+    Ok(ClawApiClient::from_auth(auth).with_base_url(base_url))
+}
+
+fn build_openai_compat_client(
+    provider_override: Option<&ProviderOverride>,
+    config: OpenAiCompatConfig,
+) -> Result<OpenAiCompatClient, ApiError> {
+    let Some(provider_override) = provider_override else {
+        return OpenAiCompatClient::from_env(config);
+    };
+
+    let api_key_env = provider_override
+        .api_key_env
+        .as_deref()
+        .unwrap_or(config.api_key_env);
+    let api_key = read_named_env_non_empty(api_key_env)?.ok_or_else(|| {
+        ApiError::Auth(format!(
+            "provider config requires credential environment variable {api_key_env}"
+        ))
+    })?;
+    let base_url = provider_override
+        .base_url
+        .clone()
+        .unwrap_or_else(|| openai_compat::read_base_url(config));
+    Ok(OpenAiCompatClient::new(api_key, config).with_base_url(base_url))
+}
+
+fn resolve_claw_auth_from_override(
+    provider_override: &ProviderOverride,
+) -> Result<AuthSource, ApiError> {
+    let api_key = provider_override
+        .api_key_env
+        .as_deref()
+        .map(read_named_env_non_empty)
+        .transpose()?
+        .flatten();
+    let auth_token = provider_override
+        .auth_token_env
+        .as_deref()
+        .map(read_named_env_non_empty)
+        .transpose()?
+        .flatten();
+
+    match (api_key, auth_token) {
+        (Some(api_key), Some(bearer_token)) => Ok(AuthSource::ApiKeyAndBearer {
+            api_key,
+            bearer_token,
+        }),
+        (Some(api_key), None) => Ok(AuthSource::ApiKey(api_key)),
+        (None, Some(bearer_token)) => Ok(AuthSource::BearerToken(bearer_token)),
+        (None, None) => Err(ApiError::Auth(
+            "provider config requires anthropic credentials from the configured env vars"
+                .to_string(),
+        )),
+    }
+}
+
+fn read_named_env_non_empty(key: &str) -> Result<Option<String>, ApiError> {
+    match std::env::var(key) {
+        Ok(value) if !value.is_empty() => Ok(Some(value)),
+        Ok(_) | Err(std::env::VarError::NotPresent) => Ok(None),
+        Err(error) => Err(ApiError::from(error)),
     }
 }
 

rust/crates/api/tests/client_integration.rs
@@ -237,6 +237,49 @@ async fn provider_client_dispatches_api_requests() {
 }
 
 #[tokio::test]
+async fn provider_client_honors_anthropic_base_url_with_explicit_auth() {
+    let state = Arc::new(Mutex::new(Vec::<CapturedRequest>::new()));
+    let server = spawn_server(
+        state.clone(),
+        vec![http_response(
+            "200 OK",
+            "application/json",
+            "{\"id\":\"msg_provider_env_base\",\"type\":\"message\",\"role\":\"assistant\",\"content\":[{\"type\":\"text\",\"text\":\"Env base URL\"}],\"model\":\"claude-sonnet-4-6\",\"stop_reason\":\"end_turn\",\"stop_sequence\":null,\"usage\":{\"input_tokens\":3,\"output_tokens\":2}}",
+        )],
+    )
+    .await;
+
+    let original_base_url = std::env::var_os("ANTHROPIC_BASE_URL");
+    std::env::set_var("ANTHROPIC_BASE_URL", server.base_url());
+
+    let client = ProviderClient::from_model_with_default_auth(
+        "claude-sonnet-4-6",
+        Some(AuthSource::ApiKey("test-key".to_string())),
+    )
+    .expect("provider client should honor env base URL");
+
+    let response = client
+        .send_message(&sample_request(false))
+        .await
+        .expect("provider-dispatched request should succeed");
+
+    match original_base_url {
+        Some(value) => std::env::set_var("ANTHROPIC_BASE_URL", value),
+        None => std::env::remove_var("ANTHROPIC_BASE_URL"),
+    }
+
+    assert_eq!(response.total_tokens(), 5);
+
+    let captured = state.lock().await;
+    let request = captured.first().expect("server should capture request");
+    assert_eq!(request.path, "/v1/messages");
+    assert_eq!(
+        request.headers.get("x-api-key").map(String::as_str),
+        Some("test-key")
+    );
+}
+
+#[tokio::test]
 async fn surfaces_retry_exhaustion_for_persistent_retryable_errors() {
     let state = Arc::new(Mutex::new(Vec::<CapturedRequest>::new()));
     let server = spawn_server(

rust/crates/api/tests/provider_client_integration.rs
@@ -1,3 +1,4 @@
+use std::cell::Cell;
 use std::ffi::OsString;
 use std::sync::{Mutex, OnceLock};
 
@@ -11,6 +12,17 @@ fn provider_client_routes_grok_aliases_through_xai() {
     let client = ProviderClient::from_model("grok-mini").expect("grok alias should resolve");
 
     assert_eq!(client.provider_kind(), ProviderKind::Xai);
+}
+
+#[test]
+fn provider_client_routes_gpt_models_through_openai() {
+    let _lock = env_lock();
+    let _openai_api_key = EnvVarGuard::set("OPENAI_API_KEY", Some("openai-test-key"));
+    let _anthropic_api_key = EnvVarGuard::set("ANTHROPIC_API_KEY", Some("claw-test-key"));
+
+    let client = ProviderClient::from_model("gpt-4.1").expect("gpt models should resolve");
+
+    assert_eq!(client.provider_kind(), ProviderKind::OpenAi);
 }
 
 #[test]
@@ -43,6 +55,40 @@ fn provider_client_uses_explicit_auth_without_env_lookup() {
     .expect("explicit auth should avoid env lookup");
 
     assert_eq!(client.provider_kind(), ProviderKind::ClawApi);
+}
+
+#[test]
+fn provider_client_skips_claw_auth_resolver_for_grok_models() {
+    let _lock = env_lock();
+    let _xai_api_key = EnvVarGuard::set("XAI_API_KEY", Some("xai-test-key"));
+    let resolver_called = Cell::new(false);
+
+    let client = ProviderClient::from_model_with_claw_auth_resolver("grok-3", None, || {
+        resolver_called.set(true);
+        Err(ApiError::Auth("resolver should not be called".to_string()))
+    })
+    .expect("xAI models should not consult Claw auth");
+
+    assert_eq!(client.provider_kind(), ProviderKind::Xai);
+    assert!(!resolver_called.get());
+}
+
+#[test]
+fn provider_client_uses_claw_auth_resolver_for_claw_models() {
+    let _lock = env_lock();
+    let _api_key = EnvVarGuard::set("ANTHROPIC_API_KEY", None);
+    let _auth_token = EnvVarGuard::set("ANTHROPIC_AUTH_TOKEN", None);
+    let resolver_called = Cell::new(false);
+
+    let client =
+        ProviderClient::from_model_with_claw_auth_resolver("claude-sonnet-4-6", None, || {
+            resolver_called.set(true);
+            Ok(AuthSource::ApiKey("claw-test-key".to_string()))
+        })
+        .expect("Claw models should use resolver auth");
+
+    assert_eq!(client.provider_kind(), ProviderKind::ClawApi);
+    assert!(resolver_called.get());
 }
 
 #[test]

esp3j0/appfs-agent

rust/crates/api/src/client.rs
@@ -2,8 +2,22 @@ use crate::error::ApiError;
 use crate::prompt_cache::{PromptCache, PromptCacheRecord, PromptCacheStats};
 use crate::providers::anthropic::{self, AnthropicClient, AuthSource};
 use crate::providers::openai_compat::{self, OpenAiCompatClient, OpenAiCompatConfig};
-use crate::providers::{self, ProviderKind};
+use crate::providers::{self, Provider, ProviderKind};
 use crate::types::{MessageRequest, MessageResponse, StreamEvent};
+
+async fn send_via_provider<P: Provider>(
+    provider: &P,
+    request: &MessageRequest,
+) -> Result<MessageResponse, ApiError> {
+    provider.send_message(request).await
+}
+
+async fn stream_via_provider<P: Provider>(
+    provider: &P,
+    request: &MessageRequest,
+) -> Result<P::Stream, ApiError> {
+    provider.stream_message(request).await
+}
 
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Clone)]
@@ -13,21 +27,74 @@ pub enum ProviderClient {
     OpenAi(OpenAiCompatClient),
 }
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct ProviderOverride {
+    pub provider: ProviderKind,
+    pub base_url: Option<String>,
+    pub api_key_env: Option<String>,
+    pub auth_token_env: Option<String>,
+}
+
 impl ProviderClient {
     pub fn from_model(model: &str) -> Result<Self, ApiError> {
-        Self::from_model_with_anthropic_auth(model, None)
+        Self::from_model_with_provider_override(model, None)
     }
 
     pub fn from_model_with_anthropic_auth(
         model: &str,
         anthropic_auth: Option<AuthSource>,
     ) -> Result<Self, ApiError> {
+        Self::from_model_with_anthropic_auth_resolver(model, None, || {
+            anthropic_auth.ok_or_else(|| ApiError::Auth("missing anthropic auth".to_string()))
+        })
+    }
+
+    pub fn from_model_with_default_auth(
+        model: &str,
+        anthropic_auth: Option<AuthSource>,
+    ) -> Result<Self, ApiError> {
+        Self::from_model_with_anthropic_auth(model, anthropic_auth)
+    }
+
+    pub fn from_model_with_provider_override(
+        model: &str,
+        provider_override: Option<&ProviderOverride>,
+    ) -> Result<Self, ApiError> {
+        Self::from_model_with_anthropic_auth_resolver(
+            model,
+            provider_override,
+            anthropic::AuthSource::from_env_or_saved,
+        )
+    }
+
+    pub fn from_model_with_claw_auth_resolver<F>(
+        model: &str,
+        provider_override: Option<&ProviderOverride>,
+        resolve_claw_auth: F,
+    ) -> Result<Self, ApiError>
+    where
+        F: FnOnce() -> Result<AuthSource, ApiError>,
+    {
+        Self::from_model_with_anthropic_auth_resolver(model, provider_override, resolve_claw_auth)
+    }
+
+    pub fn from_model_with_anthropic_auth_resolver<F>(
+        model: &str,
+        provider_override: Option<&ProviderOverride>,
+        resolve_anthropic_auth: F,
+    ) -> Result<Self, ApiError>
+    where
+        F: FnOnce() -> Result<AuthSource, ApiError>,
+    {
         let resolved_model = providers::resolve_model_alias(model);
-        match providers::detect_provider_kind(&resolved_model) {
-            ProviderKind::Anthropic => Ok(Self::Anthropic(match anthropic_auth {
-                Some(auth) => AnthropicClient::from_auth(auth),
-                None => AnthropicClient::from_env()?,
-            })),
+        let provider_kind = provider_override
+            .map(|config| config.provider)
+            .unwrap_or_else(|| providers::detect_provider_kind(&resolved_model));
+        match provider_kind {
+            ProviderKind::Anthropic => Ok(Self::Anthropic(build_anthropic_client(
+                provider_override,
+                resolve_anthropic_auth,
+            )?)),
             ProviderKind::Xai => Ok(Self::Xai(OpenAiCompatClient::from_env(
                 OpenAiCompatConfig::xai(),
             )?)),
@@ -75,8 +142,8 @@ impl ProviderClient {
         request: &MessageRequest,
     ) -> Result<MessageResponse, ApiError> {
         match self {
-            Self::Anthropic(client) => client.send_message(request).await,
-            Self::Xai(client) | Self::OpenAi(client) => client.send_message(request).await,
+            Self::Anthropic(client) => send_via_provider(client, request).await,
+            Self::Xai(client) | Self::OpenAi(client) => send_via_provider(client, request).await,
         }
     }
 
@@ -85,15 +152,101 @@ impl ProviderClient {
         request: &MessageRequest,
     ) -> Result<MessageStream, ApiError> {
         match self {
-            Self::Anthropic(client) => client
-                .stream_message(request)
+            Self::Anthropic(client) => stream_via_provider(client, request)
                 .await
                 .map(MessageStream::Anthropic),
-            Self::Xai(client) | Self::OpenAi(client) => client
-                .stream_message(request)
+            Self::Xai(client) | Self::OpenAi(client) => stream_via_provider(client, request)
                 .await
                 .map(MessageStream::OpenAiCompat),
         }
+    }
+}
+
+fn build_anthropic_client<F>(
+    provider_override: Option<&ProviderOverride>,
+    resolve_default_auth: F,
+) -> Result<AnthropicClient, ApiError>
+where
+    F: FnOnce() -> Result<AuthSource, ApiError>,
+{
+    let Some(provider_override) = provider_override else {
+        return Ok(
+            AnthropicClient::from_auth(resolve_default_auth()?).with_base_url(read_base_url())
+        );
+    };
+
+    let auth = if provider_override.api_key_env.is_none() && provider_override.auth_token_env.is_none()
+    {
+        resolve_default_auth()?
+    } else {
+        resolve_anthropic_auth_from_override(provider_override)?
+    };
+    let base_url = provider_override
+        .base_url
+        .clone()
+        .unwrap_or_else(read_base_url);
+    Ok(AnthropicClient::from_auth(auth).with_base_url(base_url))
+}
+
+fn build_openai_compat_client(
+    provider_override: Option<&ProviderOverride>,
+    config: OpenAiCompatConfig,
+) -> Result<OpenAiCompatClient, ApiError> {
+    let Some(provider_override) = provider_override else {
+        return OpenAiCompatClient::from_env(config);
+    };
+
+    let api_key_env = provider_override
+        .api_key_env
+        .as_deref()
+        .unwrap_or(config.api_key_env);
+    let api_key = read_named_env_non_empty(api_key_env)?.ok_or_else(|| {
+        ApiError::Auth(format!(
+            "provider config requires credential environment variable {api_key_env}"
+        ))
+    })?;
+    let base_url = provider_override
+        .base_url
+        .clone()
+        .unwrap_or_else(|| openai_compat::read_base_url(config));
+    Ok(OpenAiCompatClient::new(api_key, config).with_base_url(base_url))
+}
+
+fn resolve_anthropic_auth_from_override(
+    provider_override: &ProviderOverride,
+) -> Result<AuthSource, ApiError> {
+    let api_key = provider_override
+        .api_key_env
+        .as_deref()
+        .map(read_named_env_non_empty)
+        .transpose()?
+        .flatten();
+    let auth_token = provider_override
+        .auth_token_env
+        .as_deref()
+        .map(read_named_env_non_empty)
+        .transpose()?
+        .flatten();
+
+    match (api_key, auth_token) {
+        (Some(api_key), Some(bearer_token)) => Ok(AuthSource::ApiKeyAndBearer {
+            api_key,
+            bearer_token,
+        }),
+        (Some(api_key), None) => Ok(AuthSource::ApiKey(api_key)),
+        (None, Some(bearer_token)) => Ok(AuthSource::BearerToken(bearer_token)),
+        (None, None) => Err(ApiError::Auth(
+            "provider config requires anthropic credentials from the configured env vars"
+                .to_string(),
+        )),
+    }
+}
+
+fn read_named_env_non_empty(key: &str) -> Result<Option<String>, ApiError> {
+    match std::env::var(key) {
+        Ok(value) if !value.is_empty() => Ok(Some(value)),
+        Ok(_) | Err(std::env::VarError::NotPresent) => Ok(None),
+        Err(error) => Err(ApiError::from(error)),
     }
 }
 

rust/crates/api/tests/client_integration.rs
@@ -429,6 +429,49 @@ async fn provider_client_dispatches_anthropic_requests() {
 }
 
 #[tokio::test]
+async fn provider_client_honors_anthropic_base_url_with_explicit_auth() {
+    let state = Arc::new(Mutex::new(Vec::<CapturedRequest>::new()));
+    let server = spawn_server(
+        state.clone(),
+        vec![http_response(
+            "200 OK",
+            "application/json",
+            "{\"id\":\"msg_provider_env_base\",\"type\":\"message\",\"role\":\"assistant\",\"content\":[{\"type\":\"text\",\"text\":\"Env base URL\"}],\"model\":\"claude-sonnet-4-6\",\"stop_reason\":\"end_turn\",\"stop_sequence\":null,\"usage\":{\"input_tokens\":3,\"output_tokens\":2}}",
+        )],
+    )
+    .await;
+
+    let original_base_url = std::env::var_os("ANTHROPIC_BASE_URL");
+    std::env::set_var("ANTHROPIC_BASE_URL", server.base_url());
+
+    let client = ProviderClient::from_model_with_anthropic_auth(
+        "claude-sonnet-4-6",
+        Some(AuthSource::ApiKey("test-key".to_string())),
+    )
+    .expect("provider client should honor env base URL");
+
+    let response = client
+        .send_message(&sample_request(false))
+        .await
+        .expect("provider-dispatched request should succeed");
+
+    match original_base_url {
+        Some(value) => std::env::set_var("ANTHROPIC_BASE_URL", value),
+        None => std::env::remove_var("ANTHROPIC_BASE_URL"),
+    }
+
+    assert_eq!(response.total_tokens(), 5);
+
+    let captured = state.lock().await;
+    let request = captured.first().expect("server should capture request");
+    assert_eq!(request.path, "/v1/messages");
+    assert_eq!(
+        request.headers.get("x-api-key").map(String::as_str),
+        Some("test-key")
+    );
+}
+
+#[tokio::test]
 async fn surfaces_retry_exhaustion_for_persistent_retryable_errors() {
     let state = Arc::new(Mutex::new(Vec::<CapturedRequest>::new()));
     let server = spawn_server(

rust/crates/api/tests/provider_client_integration.rs
@@ -1,3 +1,4 @@
+use std::cell::Cell;
 use std::ffi::OsString;
 use std::sync::{Mutex, OnceLock};
 
@@ -11,6 +12,17 @@ fn provider_client_routes_grok_aliases_through_xai() {
     let client = ProviderClient::from_model("grok-mini").expect("grok alias should resolve");
 
     assert_eq!(client.provider_kind(), ProviderKind::Xai);
+}
+
+#[test]
+fn provider_client_routes_gpt_models_through_openai() {
+    let _lock = env_lock();
+    let _openai_api_key = EnvVarGuard::set("OPENAI_API_KEY", Some("openai-test-key"));
+    let _anthropic_api_key = EnvVarGuard::set("ANTHROPIC_API_KEY", Some("claw-test-key"));
+
+    let client = ProviderClient::from_model("gpt-4.1").expect("gpt models should resolve");
+
+    assert_eq!(client.provider_kind(), ProviderKind::OpenAi);
 }
 
 #[test]
@@ -43,6 +55,40 @@ fn provider_client_uses_explicit_anthropic_auth_without_env_lookup() {
     .expect("explicit anthropic auth should avoid env lookup");
 
     assert_eq!(client.provider_kind(), ProviderKind::Anthropic);
+}
+
+#[test]
+fn provider_client_skips_anthropic_auth_resolver_for_grok_models() {
+    let _lock = env_lock();
+    let _xai_api_key = EnvVarGuard::set("XAI_API_KEY", Some("xai-test-key"));
+    let resolver_called = Cell::new(false);
+
+    let client = ProviderClient::from_model_with_anthropic_auth_resolver("grok-3", None, || {
+        resolver_called.set(true);
+        Err(ApiError::Auth("resolver should not be called".to_string()))
+    })
+    .expect("xAI models should not consult Claw auth");
+
+    assert_eq!(client.provider_kind(), ProviderKind::Xai);
+    assert!(!resolver_called.get());
+}
+
+#[test]
+fn provider_client_uses_anthropic_auth_resolver_for_anthropic_models() {
+    let _lock = env_lock();
+    let _api_key = EnvVarGuard::set("ANTHROPIC_API_KEY", None);
+    let _auth_token = EnvVarGuard::set("ANTHROPIC_AUTH_TOKEN", None);
+    let resolver_called = Cell::new(false);
+
+    let client =
+        ProviderClient::from_model_with_anthropic_auth_resolver("claude-sonnet-4-6", None, || {
+            resolver_called.set(true);
+            Ok(AuthSource::ApiKey("anthropic-test-key".to_string()))
+        })
+        .expect("Anthropic models should use resolver auth");
+
+    assert_eq!(client.provider_kind(), ProviderKind::Anthropic);
+    assert!(resolver_called.get());
 }
 
 #[test]